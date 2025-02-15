f17cc4852e6608cf5b0177b380e71e33314fc8b5
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-02-15 22:57:22 +0900

    add pwm

--- pwm-arduino_core_ch32/pwm-arduino_core_ch32.ino
+++ pwm-arduino_core_ch32/pwm-arduino_core_ch32.ino
@@ -1,5 +1,5 @@
-#define LED1 PC1
-#define LED2 PC2
+#define LED1 PD2
+#define LED2 PD4
 
 void setup() {
   pinMode(LED1, OUTPUT);
@@ -9,15 +9,15 @@ void setup() {
 void loop() {
   printf("up\r\n");
   for(int i=0;i<16;i++){
-    analogWrite(LED1, 16*i-1);
+    analogWrite(LED1, 256*i-1);
+    analogWrite(LED2, 256*(15-i)-1);
     delay(62);
   }
-  digitalWrite(LED2, HIGH);
 
   printf("down\r\n");
-  for(int i=16;i>0;i--){
-    analogWrite(LED1, 16*i-1);
+  for(int i=0;i<16;i++){
+    analogWrite(LED1, 256*(15-i)-1);
+    analogWrite(LED2, 256*i-1);
     delay(62);
   }
-  digitalWrite(LED2, LOW);
 }
