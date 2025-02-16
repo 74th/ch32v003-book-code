7be0eedddab1b9d866d6f7913f59797a5c30814c
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-02-16 09:48:29 +0900

    gpio

--- lchika-arduino_core_ch32/lchika-arduino_core_ch32.ino
+++ lchika-arduino_core_ch32/lchika-arduino_core_ch32.ino
@@ -1,12 +1,31 @@
 #define LED PC0
+#define BUTTON PA1
 
-void setup() {
+void setup()
+{
+  // 動作を確認できなかった
+  USART_Printf_Init(115200);
+
+  printf("init\r\n");
+
   pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
+
+  printf("start\r\n");
 }
 
-void loop() {
+void loop()
+{
+  printf("loop\r\n");
+  
+  bool btn = digitalRead(BUTTON);
+
+  if(!btn){
+    return;
+  }
+
   digitalWrite(LED, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(LED, LOW);
-  delay(1000);
+  delay(500);
 }
