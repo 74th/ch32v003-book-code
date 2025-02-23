bee5a2158744efb8811f148bf6b417b688b2ac1f
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-02-23 15:32:01 +0900

    i2c master

--- i2c_master-arduino_ch32v003/i2c_master-arduino_ch32v003.ino
+++ i2c_master-arduino_ch32v003/i2c_master-arduino_ch32v003.ino
@@ -1,37 +1,30 @@
-#include "Wire.h"
+#include "hal/wch-hal-i2c.h"
 
 #define SHT31_I2C_ADDR 0x44
 
 void setup()
 {
+  // UART
   Serial.begin(115200);
-
-  // not working - 動作を確認できなかった
-  // USART_Printf_Init(115200);
-
   Serial.println("init");
-  // printf("init\r\n");
 
-  Wire.begin();
+  // minichlink -T
+  printf("init\r\n");
+
+  i2c_init(80000, I2C_DutyCycle_16_9, 0x02, I2C_Ack_Enable, I2C_AcknowledgedAddress_7bit);
 
   // ソフトリセット
-  Wire.beginTransmission(SHT31_I2C_ADDR);
-  Wire.write(0x30);
-  Wire.write(0xA2);
-  Wire.endTransmission();
-
+  uint8_t buf1[2] = {0x30, 0xA2};
+  i2c_write(SHT31_I2C_ADDR, buf1, 2);
   delay(300);
 
   // ステータスレジスタ消去
-  Wire.beginTransmission(SHT31_I2C_ADDR);
-  Wire.write(0x30);
-  Wire.write(0x41);
-  Wire.endTransmission();
-
+  uint8_t buf2[2] = {0x30, 0x41};
+  i2c_write(SHT31_I2C_ADDR, buf2, 2);
   delay(300);
 
   Serial.println("start");
-  // printf("start\r\n");
+  printf("start\r\n");
 }
 
 uint32_t count = 0;
@@ -40,31 +33,25 @@ void loop()
 {
   Serial.print("loop ");
   Serial.println(count++);
-  // printf("loop %d\r\n", count);
+  printf("loop %d\r\n", count);
 
-  uint8_t dac[6] = {0};
+  uint8_t dac[6];
   int i, t, h;
   uint32_t temperature, humidity;
 
-  Wire.beginTransmission(SHT31_I2C_ADDR);
-  Wire.write(0x24);
-  Wire.write(0x00);
-  Wire.endTransmission();
+  uint8_t buf[2] = {0x24, 0x00};
+  i2c_write(SHT31_I2C_ADDR, buf, 2);
 
   delay(300);
 
-  Wire.requestFrom(SHT31_I2C_ADDR, 6);
-  for (i = 0; i < 6; i++)
-  {
-    dac[i] = Wire.read();
-  }
-  Wire.endTransmission();
+  i2c_read(SHT31_I2C_ADDR, dac, 6);
 
   t = (dac[0] << 8) | dac[1];                       // 1Byte目のデータを8bit左にシフト、OR演算子で2Byte目のデータを結合して、tに代入
   temperature = (((uint32_t)(t) * 175) >> 16) - 45; // 温度の計算、temperatureに代入
   h = (dac[3] << 8) | dac[4];                       // 4Byte目のデータを8bit左にシフト、OR演算子で5Byte目のデータを結合して、hに代入
   humidity = (((uint32_t)(h)*100) >> 16);           // 湿度の計算、humidityに代入
 
+  printf("temperature: %d humidity: %d\r\n", temperature, humidity);
   Serial.print("temperature: ");
   Serial.print(temperature);
   Serial.print(" humidity: ");
