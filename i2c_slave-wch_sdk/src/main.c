ae780bb53cfaf5ae13b461eb6d36914d3a43deec
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-03-16 20:48:39 +0900

    I2C Slave WIp

---
+++ i2c_slave-wch_sdk/src/main.c
@@ -0,0 +1,177 @@
+#include <ch32v00x.h>
+#include <debug.h>
+
+#define I2C_ADDRESS 0x10
+
+void I2C1_EV_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void I2C1_ER_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+
+void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void Delay_Init(void);
+void Delay_Ms(uint32_t n);
+
+volatile uint8_t i2c_registers[0x30] = {0x00};
+volatile uint8_t i2c_position = 0;
+uint8_t i2c_first_receive = 0;
+
+void I2C1_EV_IRQHandler(void)
+{
+    uint16_t STAR1, STAR2 __attribute__((unused));
+    STAR1 = I2C1->STAR1;
+    STAR2 = I2C1->STAR2;
+
+    I2C1->CTLR1 |= I2C_CTLR1_ACK;
+
+    if (STAR1 & I2C_STAR1_ADDR) // 0x0002
+    {
+        // 最初のイベント
+        // read でも write でも必ず最初に呼ばれる
+        i2c_first_receive = 1;
+        i2c_position = 0;
+    }
+
+    if (STAR1 & I2C_STAR1_RXNE) // 0x0040
+    {
+        // 1byte 受信
+        uint8_t v = I2C1->DATAR;
+        if (i2c_first_receive)
+        {
+            // 1バイト目の受信
+            i2c_position = v;
+            i2c_first_receive = 0;
+        }
+        else if (i2c_position < sizeof(i2c_registers))
+        {
+            // 2バイト目以降の受信
+            i2c_registers[i2c_position] = v;
+            i2c_position++;
+        }
+        else
+        {
+            // 2バイト目以降の受信、レジスタ範囲外
+            // 何もしない
+        }
+    }
+
+    if (STAR1 & I2C_STAR1_TXE) // 0x0080
+    {
+        // 1byte リクエスト
+        if (i2c_position < sizeof(i2c_registers))
+        {
+            // 1byte 送信
+            // I2C_SendData(I2C1, i2c_registers[i2c_position]);
+            i2c_position++;
+        }
+        else
+        {
+            // 1byte 送信
+            // I2C_SendData(I2C1, 0x00);
+            I2C1->DATAR = 0x00;
+        }
+    }
+}
+
+void I2C1_ER_IRQHandler(void)
+{
+    uint16_t STAR1 = I2C1->STAR1;
+
+    if (STAR1 & I2C_STAR1_BERR)
+    {
+        I2C1->STAR1 &= ~(I2C_STAR1_BERR);
+    }
+
+    if (STAR1 & I2C_STAR1_ARLO)
+    {
+        I2C1->STAR1 &= ~(I2C_STAR1_ARLO);
+    }
+
+    if (STAR1 & I2C_STAR1_AF)
+    {
+        I2C1->STAR1 &= ~(I2C_STAR1_AF);
+    }
+}
+
+int main(void)
+{
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+    SystemCoreClockUpdate();
+    Delay_Init();
+
+    // SDI printf の有効化
+    // ビルドフラグ SDI_PRINT=1 が必要
+    // wlink sdi-print enable を実行し、USBシリアルをリスンする
+    // SDI_Printf_Enable();
+
+    // USART1 printf の有効化
+    USART_Printf_Init(115200);
+
+    Delay_Ms(100);
+
+    printf("init\r\n");
+
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+    I2C_InitTypeDef I2C_InitTSturcture = {0};
+    NVIC_InitTypeDef NVIC_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+
+    // PC1: SDA, PC2: SCL
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    NVIC_InitStructure.NVIC_IRQChannel = I2C1_EV_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    NVIC_InitStructure.NVIC_IRQChannel = I2C1_ER_IRQn;
+    NVIC_Init(&NVIC_InitStructure);
+
+    // I2C1 Init
+    I2C_InitTSturcture.I2C_ClockSpeed = 2000000;
+    I2C_InitTSturcture.I2C_Mode = I2C_Mode_I2C;
+    I2C_InitTSturcture.I2C_DutyCycle = I2C_DutyCycle_2;
+    I2C_InitTSturcture.I2C_OwnAddress1 = I2C_ADDRESS << 1;
+    // I2C_InitTSturcture.I2C_OwnAddress1 = I2C_ADDRESS;
+    I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+    I2C_Init(I2C1, &I2C_InitTSturcture);
+
+    I2C_Cmd(I2C1, ENABLE);
+    I2C_AcknowledgeConfig(I2C1, ENABLE);
+
+    I2C_ITConfig(I2C1, I2C_IT_EVT, ENABLE);
+    I2C_ITConfig(I2C1, I2C_IT_ERR, ENABLE);
+    I2C_ITConfig(I2C1, I2C_IT_BUF, ENABLE);
+    NVIC_EnableIRQ(I2C1_EV_IRQn);
+    NVIC_SetPriority(I2C1_EV_IRQn, 2 << 4);
+    NVIC_EnableIRQ(I2C1_ER_IRQn);
+
+    // printf("I2C1->CTLR1: %4x\r\n", I2C1->CTLR1);
+    // printf("I2C1->CTLR2: %4x\r\n", I2C1->CTLR2);
+    // printf("I2C1->CKCFGR: %4x\r\n", I2C1->CKCFGR);
+    // printf("I2C1->OADDR1: %4x\r\n", I2C1->OADDR1);
+
+    Delay_Ms(100);
+
+    printf("start\r\n");
+
+    int count = 0;
+
+    while (1)
+    {
+    }
+}
+
+void NMI_Handler(void) {}
+void HardFault_Handler(void)
+{
+    while (1)
+    {
+    }
+}
