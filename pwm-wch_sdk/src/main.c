2a6ee2ebc8f74eae5bb3ad2f63442ed3bc618480
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-04-05 17:36:09 +0900

    GPIO設定のリファクタリングとPWM出力の初期化処理を改善

--- pwm-wch_sdk/src/main.c
+++ pwm-wch_sdk/src/main.c
@@ -1,7 +1,7 @@
 #include <ch32v00x.h>
 #include <debug.h>
 
-#define LED_PIN GPIO_Pin_2
+#define USE_REMAP 1
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -14,13 +14,42 @@ void TIM1_PWMOut_Init()
     TIM_OCInitTypeDef TIM_OCInitStructure = {0};
     TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure = {0};
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_TIM1, ENABLE);
+    // GPIO、Timerのクロック有効化
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_TIM1, ENABLE);
 
-    // PD2をAF出力設定
+#if USE_REMAP
+    // AFIOクロック有効化
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+#endif
+
+#if USE_REMAP
+    // Partial Remap 1を適用
+    GPIO_PinRemapConfig(GPIO_PartialRemap1_TIM1, ENABLE);
+
+    // TIM1_CH1_1 -> PC6
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+    // TIM1_CH4_1 -> PD3
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+#else
+    // TIM1_CH1 -> PD2
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
     GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    // TIM1_CH4 -> PC4
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
+#endif
 
     // Timer1を256周期で有効か
     TIM_TimeBaseInitStructure.TIM_Period = 255;
@@ -36,8 +65,16 @@ void TIM1_PWMOut_Init()
     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
     TIM_OC1Init(TIM1, &TIM_OCInitStructure);
 
+    // Timer1 CH4をPWMで有効化
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_Pulse = 0;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OC4Init(TIM1, &TIM_OCInitStructure);
+
     TIM_CtrlPWMOutputs(TIM1, ENABLE);
     TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Disable);
+    TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Disable);
     TIM_ARRPreloadConfig(TIM1, ENABLE);
     TIM_Cmd(TIM1, ENABLE);
 }
@@ -48,7 +85,11 @@ int main(void)
     SystemCoreClockUpdate();
     Delay_Init();
 
+#if SDI_PRINT
     SDI_Printf_Enable();
+#else
+    USART_Printf_Init(115200);
+#endif
 
     printf("init\r\n");
 
@@ -63,7 +104,7 @@ int main(void)
         for (int i = 1; i <= 32; i++)
         {
             TIM_SetCompare1(TIM1, 8 * i - 1);
-            // TIM_SetCompare2(TIM1, 128 * (33 - i) - 1);
+            TIM_SetCompare4(TIM1, 128 * (33 - i) - 1);
             Delay_Us(1000000 / 32);
         }
 
@@ -71,7 +112,7 @@ int main(void)
         for (int i = 1; i <= 32; i++)
         {
             TIM_SetCompare1(TIM1, 8 * (33 - i) - 1);
-            // TIM_SetCompare2(TIM1, 8 * i - 1);
+            TIM_SetCompare4(TIM1, 8 * i - 1);
             Delay_Us(1000000 / 32);
         }
     }
