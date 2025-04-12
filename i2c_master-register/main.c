0e506144742eabe1ad4ba664cf0979079c9f0566
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-04-12 10:49:16 +0900

    インデント修正

--- i2c_master-register/main.c
+++ i2c_master-register/main.c
@@ -4,10 +4,16 @@
 #define I2C_SLAVE_ADDRESS 0x44
 #define LOOP_MS 1000
 
+#define USE_REMAP 1
+
 void init_rcc(void)
 {
-	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD;
-	RCC->APB1PCENR |= RCC_APB1Periph_I2C1;
+  // クロック供給の有効化
+  RCC->APB2PCENR |= RCC_APB2Periph_GPIOC;
+  RCC->APB1PCENR |= RCC_APB1Periph_I2C1;
+#if USE_REMAP
+  RCC->APB2PCENR |= RCC_APB2Periph_AFIO;
+#endif
 }
 
 /**
@@ -18,320 +24,335 @@ void init_rcc(void)
 void init_i2c_master()
 {
 
-	// PC1 is SDA, 10MHz Output, alt func, open-drain
-	GPIOC->CFGLR &= ~(0xf << (4 * 1));
-	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_OD_AF) << (4 * 1);
+#if USE_REMAP
+  // リマップ設定リセット
+  AFIO->PCFR1 &= ~(AFIO_PCFR1_I2C1_REMAP | AFIO_PCFR1_I2C1_HIGH_BIT_REMAP);
+  // リマップ設定
+  // Remap1: AFIO_PCFR1_I2C1_REMAP
+  // Remap2: AFIO_PCFR1_I2C1_HIGH_BIT_REMAP
+  AFIO->PCFR1 |= AFIO_PCFR1_I2C1_HIGH_BIT_REMAP;
 
-	// PC2 is SCL, 10MHz Output, alt func, open-drain
-	GPIOC->CFGLR &= ~(0xf << (4 * 2));
-	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_OD_AF) << (4 * 2);
+  // PC6: SDA
+  GPIOC->CFGLR &= ~(0xf << (4 * 6));
+  GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_OD_AF) << (4 * 6);
 
-	uint16_t tempreg;
+  // PC5: SCL
+  GPIOC->CFGLR &= ~(0xf << (4 * 5));
+  GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_OD_AF) << (4 * 5);
+#else
+  // PC1: SDA
+  GPIOC->CFGLR &= ~(0xf << (4 * 1));
+  GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_OD_AF) << (4 * 1);
 
-	// Reset I2C1 to init all regs
-	RCC->APB1PRSTR |= RCC_APB1Periph_I2C1;
-	RCC->APB1PRSTR &= ~RCC_APB1Periph_I2C1;
+  // PC2: SCL
+  GPIOC->CFGLR &= ~(0xf << (4 * 2));
+  GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_OD_AF) << (4 * 2);
+#endif
 
-	I2C1->CTLR1 |= I2C_CTLR1_SWRST;
-	I2C1->CTLR1 &= ~I2C_CTLR1_SWRST;
+  uint16_t tempreg;
 
-	uint32_t prerate = 2000000;
-	uint32_t clkrate = 1000000;
+  I2C1->CTLR1 |= I2C_CTLR1_SWRST;
+  I2C1->CTLR1 &= ~I2C_CTLR1_SWRST;
 
-	// Set module clock frequency
-	tempreg = I2C1->CTLR2;
-	tempreg &= ~I2C_CTLR2_FREQ;
-	tempreg |= (FUNCONF_SYSTEM_CORE_CLOCK / prerate) & I2C_CTLR2_FREQ;
-	I2C1->CTLR2 = tempreg;
+  uint32_t clkrate = 400000;
+  uint32_t prerate = clkrate * 2;
 
-	// Set clock config
-	tempreg = 0;
-	tempreg = (FUNCONF_SYSTEM_CORE_CLOCK / (25 * clkrate)) & I2C_CKCFGR_CCR;
-	tempreg |= I2C_CKCFGR_DUTY;
-	tempreg |= I2C_CKCFGR_FS;
-	I2C1->CKCFGR = tempreg;
+  // 回路のクロック周波数を設定
+  tempreg = I2C1->CTLR2;
+  tempreg &= ~I2C_CTLR2_FREQ;
+  tempreg |= (FUNCONF_SYSTEM_CORE_CLOCK / prerate) & I2C_CTLR2_FREQ;
+  I2C1->CTLR2 = tempreg;
 
-	// Enable I2C
-	I2C1->CTLR1 |= I2C_CTLR1_PE;
+  // I2Cクロックスピードの設定
+  tempreg = I2C1->CKCFGR;
+  tempreg &= ~I2C_CKCFGR_CCR;
+  tempreg = (FUNCONF_SYSTEM_CORE_CLOCK / (25 * clkrate)) & I2C_CKCFGR_CCR;
+  I2C1->CKCFGR = tempreg;
 
-	// set ACK mode
-	I2C1->CTLR1 |= I2C_CTLR1_ACK;
+  I2C1->CKCFGR |=
+      // デューティーサイクル(16:9の場合1、2:1の場合は設定なし)
+      I2C_CKCFGR_DUTY |
+      // ファストモード（100kHzより大きい速度の場合指定）
+      I2C_CKCFGR_FS;
+
+  // Enable I2C
+  I2C1->CTLR1 |= I2C_CTLR1_PE;
+
+  // set ACK mode
+  I2C1->CTLR1 |= I2C_CTLR1_ACK;
 }
 
 #define TIMEOUT_MAX 100000
 // https://github.com/cnlohr/ch32v003fun/blob/ee60fd756aa015be799e430d230a8f33266421de/examples/i2c_oled/i2c.h#L115
-#define I2C_EVENT_MASTER_MODE_SELECT ((uint32_t)0x00030001)				  /* BUSY, MSL and SB flag */
+#define I2C_EVENT_MASTER_MODE_SELECT ((uint32_t)0x00030001)               /* BUSY, MSL and SB flag */
 #define I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED ((uint32_t)0x00070082) /* BUSY, MSL, ADDR, TXE and TRA flags */
-#define I2C_EVENT_MASTER_BYTE_TRANSMITTED ((uint32_t)0x00070084)		  /* TRA, BUSY, MSL, TXE and BTF flags */
-#define I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED ((uint32_t)0x00030002)	  /* BUSY, SB, ADDR flags */
-#define I2C_EVENT_MASTER_BYTE_RECEIVED ((uint32_t)0x00030040)			  /* TRA, SB, ADDR flags */
+#define I2C_EVENT_MASTER_BYTE_TRANSMITTED ((uint32_t)0x00070084)          /* TRA, BUSY, MSL, TXE and BTF flags */
+#define I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED ((uint32_t)0x00030002)    /* BUSY, SB, ADDR flags */
+#define I2C_EVENT_MASTER_BYTE_RECEIVED ((uint32_t)0x00030040)             /* TRA, SB, ADDR flags */
 
 /*
  * イベントコード
  * https://github.com/cnlohr/ch32v003fun/blob/8f7517e9acf2a3de650fc3cdd125ed1a1d01f9a2/extralibs/ssd1306_i2c.h#L125
  */
-uint8_t i2c_chk_evt(uint32_t event_mask)
+uint8_t check_i2c_event(uint32_t event_mask)
 {
-	/* read order matters here! STAR1 before STAR2!! */
-	uint32_t status = I2C1->STAR1 | (I2C1->STAR2 << 16);
-	return (status & event_mask) == event_mask;
+  /* read order matters here! STAR1 before STAR2!! */
+  uint32_t status = I2C1->STAR1 | (I2C1->STAR2 << 16);
+  return (status & event_mask) == event_mask;
 }
 
 /*
  * low-level packet send for blocking polled operation via i2c
  * https://github.com/cnlohr/ch32v003fun/blob/8f7517e9acf2a3de650fc3cdd125ed1a1d01f9a2/extralibs/ssd1306_i2c.h#L245
  */
-uint8_t i2c_send(uint8_t addr, uint8_t *data, uint8_t sz)
+// 送信
+uint8_t send_i2c_data(uint8_t address, uint8_t *data, uint8_t length)
 {
-	int32_t timeout;
+  int32_t timeout;
 
-	// I2C1->CTLR1 |= I2C_CTLR1_ACK;
+  // I2Cのバスがビジーでなくなるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && (I2C1->STAR2 & I2C_STAR2_BUSY))
+    ;
+  if (timeout < 0)
+  {
+    printf("i2c error: waiting for not BUSY is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return -1;
+  }
 
-	// wait for not busy
-	timeout = TIMEOUT_MAX;
-	while ((I2C1->STAR2 & I2C_STAR2_BUSY) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for not BUSY is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return -1;
-	}
+  // 通信の開始の送信
+  I2C1->CTLR1 |= I2C_CTLR1_START;
 
-	// Set START condition
-	I2C1->CTLR1 |= I2C_CTLR1_START;
+  // マスターモードに準備できるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && (!check_i2c_event(I2C_EVENT_MASTER_MODE_SELECT)))
+    ;
+  if (timeout < 0)
+  {
+    printf("i2c error: waiting for master select is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return -1;
+  }
 
-	// wait for master mode select
-	timeout = TIMEOUT_MAX;
-	while ((!i2c_chk_evt(I2C_EVENT_MASTER_MODE_SELECT)) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for master select is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return -1;
-	}
+  // スレーブアドレスをマスターからスレーブへの通信として送信
+  I2C1->DATAR = address << 1;
 
-	// send 7-bit address + write flag
-	I2C1->DATAR = addr << 1;
+  // トランスミッターモードに準備できるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && (!check_i2c_event(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)))
+    ;
+  if (timeout < 0)
+  {
+    printf("i2c error: waiting for transmit condition is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return -1;
+  }
 
-	// wait for transmit condition
-	timeout = TIMEOUT_MAX;
-	while ((!i2c_chk_evt(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for transmit condition is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return -1;
-	}
+  for (int i = 0; i < length; i++)
+  {
+    // 1バイトのデータ送信
+    timeout = TIMEOUT_MAX;
+    while (!(I2C1->STAR1 & I2C_STAR1_TXE) && (timeout--))
+      ;
+    if (timeout < 0)
+    {
+      printf("i2c error: waiting for data send is timeout\r\n");
+      I2C1->CTLR1 |= I2C_CTLR1_STOP;
+      return -1;
+    }
 
-	// send data one byte at a time
-	while (sz--)
-	{
-		// wait for TX Empty
-		timeout = TIMEOUT_MAX;
-		while (!(I2C1->STAR1 & I2C_STAR1_TXE) && (timeout--))
-			;
-		if (timeout == -1)
-		{
-			printf("i2c error: waiting for data send is timeout\r\n");
-			I2C1->CTLR1 |= I2C_CTLR1_STOP;
-			return -1;
-		}
+    // 1バイト送信
+    I2C1->DATAR = data[i];
+  }
 
-		// send command
-		I2C1->DATAR = *data++;
-	}
+  // 送信完了イベントまで待つ
+  timeout = TIMEOUT_MAX;
+  while ((!check_i2c_event(I2C_EVENT_MASTER_BYTE_TRANSMITTED)) && (timeout--))
+    ;
+  if (timeout == -1)
+  {
+    printf("i2c error: waiting for tx complete is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return -1;
+  }
 
-	// wait for tx complete
-	timeout = TIMEOUT_MAX;
-	while ((!i2c_chk_evt(I2C_EVENT_MASTER_BYTE_TRANSMITTED)) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for tx complete is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return -1;
-	}
+  // 通信の終了の送信
+  I2C1->CTLR1 |= I2C_CTLR1_STOP;
 
-	// set STOP condition
-	I2C1->CTLR1 |= I2C_CTLR1_STOP;
-
-	// we're happy
-	return 0;
+  return 0;
 }
 
-int i2c_receive(uint8_t addr, uint8_t *buf, uint8_t sz)
+int i2c_receive(uint8_t address, uint8_t *buf, uint8_t length)
 {
-	int32_t timeout;
+  int32_t timeout;
 
-	I2C1->CTLR1 |= I2C_CTLR1_ACK;
+  I2C1->CTLR1 |= I2C_CTLR1_ACK;
 
-	// wait for not busy
-	timeout = TIMEOUT_MAX;
-	while ((I2C1->STAR2 & I2C_STAR2_BUSY) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for not BUSY is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return -1;
-	}
+  // wait for not busy
+  timeout = TIMEOUT_MAX;
+  while ((I2C1->STAR2 & I2C_STAR2_BUSY) && (timeout--))
+    ;
+  if (timeout == -1)
+  {
+    printf("i2c error: waiting for not BUSY is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return -1;
+  }
 
-	// Set START condition
-	I2C1->CTLR1 |= I2C_CTLR1_START;
+  // Set START condition
+  I2C1->CTLR1 |= I2C_CTLR1_START;
 
-	// wait for master mode select
-	timeout = TIMEOUT_MAX;
-	while ((!i2c_chk_evt(I2C_EVENT_MASTER_MODE_SELECT)) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for master select is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return -1;
-	}
+  // wait for master mode select
+  timeout = TIMEOUT_MAX;
+  while ((!check_i2c_event(I2C_EVENT_MASTER_MODE_SELECT)) && (timeout--))
+    ;
+  if (timeout == -1)
+  {
+    printf("i2c error: waiting for master select is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return -1;
+  }
 
-	// send 7-bit address + receive flag
-	I2C1->DATAR = addr << 1 | 0x1;
+  // send 7-bit address + receive flag
+  I2C1->DATAR = address << 1 | 0x1;
 
-	// wait for transmit condition
-	timeout = TIMEOUT_MAX;
-	while ((!i2c_chk_evt(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for transmit condition is timeout %04x, %04x\r\n", I2C1->STAR1, I2C1->STAR2);
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return -1;
-	}
+  // wait for transmit condition
+  timeout = TIMEOUT_MAX;
+  while ((!check_i2c_event(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) && (timeout--))
+    ;
+  if (timeout == -1)
+  {
+    printf("i2c error: waiting for transmit condition is timeout %04x, %04x\r\n", I2C1->STAR1, I2C1->STAR2);
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return -1;
+  }
 
-	// send data one byte at a time
-	while (sz--)
-	{
-		if (sz == 0)
-		{
-			printf("i2c receive last byte\r\n");
-			I2C1->CTLR1 &= ~I2C_CTLR1_ACK;
-		}
-		// wait for TX Empty
-		timeout = TIMEOUT_MAX;
-		while (!(I2C1->STAR1 & I2C_STAR1_RXNE) && (timeout--))
-			;
-		if (timeout == -1)
-		{
-			printf("i2c error: receiving for data send is timeout\r\n");
-			I2C1->CTLR1 |= I2C_CTLR1_STOP;
-			return -1;
-		}
+  // send data one byte at a time
+  while (length--)
+  {
+    if (length == 0)
+    {
+      printf("i2c receive last byte\r\n");
+      I2C1->CTLR1 &= ~I2C_CTLR1_ACK;
+    }
+    // wait for TX Empty
+    timeout = TIMEOUT_MAX;
+    while (!(I2C1->STAR1 & I2C_STAR1_RXNE) && (timeout--))
+      ;
+    if (timeout == -1)
+    {
+      printf("i2c error: receiving for data send is timeout\r\n");
+      I2C1->CTLR1 |= I2C_CTLR1_STOP;
+      return -1;
+    }
 
-		// receive command
-		*buf = I2C1->DATAR;
-		printf("i2c receive: [%2d] 0x%02x\r\n", sz, *buf);
-		buf++;
-	}
+    // receive command
+    *buf = I2C1->DATAR;
+    printf("i2c receive: [%2d] 0x%02x\r\n", length, *buf);
+    buf++;
+  }
 
-	// set STOP condition
-	I2C1->CTLR1 |= I2C_CTLR1_STOP;
+  // set STOP condition
+  I2C1->CTLR1 |= I2C_CTLR1_STOP;
 
-	return 0;
+  return 0;
 }
 
 int i2c_slave_available(uint8_t addr)
 {
-	int32_t timeout;
+  int32_t timeout;
 
-	I2C1->CTLR1 |= I2C_CTLR1_ACK;
+  I2C1->CTLR1 |= I2C_CTLR1_ACK;
 
-	// wait for not busy
-	timeout = TIMEOUT_MAX;
-	while ((I2C1->STAR2 & I2C_STAR2_BUSY) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for not BUSY is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return 0;
-	}
+  // wait for not busy
+  timeout = TIMEOUT_MAX;
+  while ((I2C1->STAR2 & I2C_STAR2_BUSY) && (timeout--))
+    ;
+  if (timeout == -1)
+  {
+    printf("i2c error: waiting for not BUSY is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return 0;
+  }
 
-	// Set START condition
-	I2C1->CTLR1 |= I2C_CTLR1_START;
+  // Set START condition
+  I2C1->CTLR1 |= I2C_CTLR1_START;
 
-	// wait for master mode select
-	timeout = TIMEOUT_MAX;
-	while ((!i2c_chk_evt(I2C_EVENT_MASTER_MODE_SELECT)) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for master select is timeout\r\n");
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return 0;
-	}
+  // wait for master mode select
+  timeout = TIMEOUT_MAX;
+  while ((!check_i2c_event(I2C_EVENT_MASTER_MODE_SELECT)) && (timeout--))
+    ;
+  if (timeout == -1)
+  {
+    printf("i2c error: waiting for master select is timeout\r\n");
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return 0;
+  }
 
-	// send 7-bit address + receive flag
-	I2C1->DATAR = addr << 1 | 0x1;
+  // send 7-bit address + receive flag
+  I2C1->DATAR = addr << 1 | 0x1;
 
-	// wait for transmit condition
-	timeout = TIMEOUT_MAX;
-	while ((!i2c_chk_evt(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) && (timeout--))
-		;
-	if (timeout == -1)
-	{
-		printf("i2c error: waiting for transmit condition is timeout %04x, %04x\r\n", I2C1->STAR1, I2C1->STAR2);
-		I2C1->CTLR1 |= I2C_CTLR1_STOP;
-		return 0;
-	}
+  // wait for transmit condition
+  timeout = TIMEOUT_MAX;
+  while ((!check_i2c_event(I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED)) && (timeout--))
+    ;
+  if (timeout == -1)
+  {
+    printf("i2c error: waiting for transmit condition is timeout %04x, %04x\r\n", I2C1->STAR1, I2C1->STAR2);
+    I2C1->CTLR1 |= I2C_CTLR1_STOP;
+    return 0;
+  }
 
-	I2C1->CTLR1 |= I2C_CTLR1_STOP;
+  I2C1->CTLR1 |= I2C_CTLR1_STOP;
 
-	return 1;
+  return 1;
 }
 
 int main()
 {
-	SystemInit();
-	init_rcc();
+  SystemInit();
+  init_rcc();
 
-	printf("init\r\n");
+  printf("init\r\n");
 
-	init_i2c_master(I2C_SLAVE_ADDRESS);
+  init_i2c_master(I2C_SLAVE_ADDRESS);
 
-	printf("setup\r\n");
+  printf("setup\r\n");
 
-	Delay_Ms(100);
+  Delay_Ms(100);
 
-	uint8_t buf1[2] = {0x30, 0xA2};
-	i2c_send(I2C_SLAVE_ADDRESS, buf1, 2);
-	Delay_Ms(300);
+  uint8_t buf1[2] = {0x30, 0xA2};
+  send_i2c_data(I2C_SLAVE_ADDRESS, buf1, 2);
+  Delay_Ms(300);
 
-	uint8_t buf2[2] = {0x30, 0x41};
-	i2c_send(I2C_SLAVE_ADDRESS, buf2, 2);
-	Delay_Ms(300);
-	printf("test i2c\r\n");
+  uint8_t buf2[2] = {0x30, 0x41};
+  send_i2c_data(I2C_SLAVE_ADDRESS, buf2, 2);
+  Delay_Ms(300);
+  printf("test i2c\r\n");
 
-	printf("test i2c done\r\n");
+  printf("test i2c done\r\n");
 
-	while (1)
-	{
-		uint8_t dac[6];
-		uint32_t t, h;
-		uint32_t temperature, humidity;
+  while (1)
+  {
+    uint8_t dac[6];
+    uint32_t t, h;
+    uint32_t temperature, humidity;
 
-		uint8_t buf[2] = {0x24, 0x00};
-		i2c_send(I2C_SLAVE_ADDRESS, buf, sizeof(buf));
+    uint8_t buf[2] = {0x24, 0x00};
+    send_i2c_data(I2C_SLAVE_ADDRESS, buf, sizeof(buf));
 
-		Delay_Ms(300);
+    Delay_Ms(300);
 
-		i2c_receive(I2C_SLAVE_ADDRESS, dac, sizeof(dac));
+    i2c_receive(I2C_SLAVE_ADDRESS, dac, sizeof(dac));
 
-		t = (dac[0] << 8) | dac[1];						  // 1Byte目のデータを8bit左にシフト、OR演算子で2Byte目のデータを結合して、tに代入
-		temperature = (((uint32_t)(t) * 175) >> 16) - 45; // 温度の計算、temperatureに代入
-		h = (dac[3] << 8) | dac[4];						  // 4Byte目のデータを8bit左にシフト、OR演算子で5Byte目のデータを結合して、hに代入
-		humidity = (((uint32_t)(h) * 100) >> 16);		  // 湿度の計算、humidityに代入
+    t = (dac[0] << 8) | dac[1];                       // 1Byte目のデータを8bit左にシフト、OR演算子で2Byte目のデータを結合して、tに代入
+    temperature = (((uint32_t)(t) * 175) >> 16) - 45; // 温度の計算、temperatureに代入
+    h = (dac[3] << 8) | dac[4];                       // 4Byte目のデータを8bit左にシフト、OR演算子で5Byte目のデータを結合して、hに代入
+    humidity = (((uint32_t)(h) * 100) >> 16);         // 湿度の計算、humidityに代入
 
-		printf("Temperature: %lu, Humidity: %lu\r\n", temperature, humidity);
+    printf("Temperature: %lu, Humidity: %lu\r\n", temperature, humidity);
 
-		Delay_Ms(5000);
-	}
+    Delay_Ms(5000);
+  }
 }
--- i2c_master-wch_sdk/src/main.c
+++ i2c_master-wch_sdk/src/main.c
@@ -5,7 +5,7 @@
 
 #define SHT31_I2C_ADDR 0x44
 
-#define USE_REMAP 1
+#define USE_REMAP 0
 
 void NMI_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
 void HardFault_Handler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
@@ -13,213 +13,240 @@ void Delay_Init(void);
 void Delay_Ms(uint32_t n);
 
 // 送信
-void send_i2c_data(uint8_t address, uint8_t *data, uint8_t length)
+int8_t send_i2c_data(uint8_t address, uint8_t *data, uint8_t length)
 {
-    int32_t timeout;
+  int32_t timeout;
 
-    // I2Cのバスがビジーでなくなるまで待つ
-    timeout = TIMEOUT_MAX;
-    while (timeout-- && I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET)
-        ;
+  // I2Cのバスがビジーでなくなるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET)
+    ;
 
+  if (timeout < 0)
+  {
+    return -1;
+  }
+
+  // 通信の開始の送信
+  I2C_GenerateSTART(I2C1, ENABLE);
+
+  // マスターモードに準備できるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT))
+    ;
+  if (timeout < 0)
+  {
+    return -1;
+  }
+
+  // スレーブアドレスをマスターからスレーブへの通信として送信
+  I2C_Send7bitAddress(I2C1, address << 1, I2C_Direction_Transmitter);
+
+  // トランスミッターモードに準備できるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED))
+    ;
+  if (timeout < 0)
+  {
+    return -1;
+  }
+
+  for (int i = 0; i < length; i++)
+  {
+    // 送信バッファが空になるまで待つ
+    while (timeout-- && I2C_GetFlagStatus(I2C1, I2C_FLAG_TXE) == RESET)
+      ;
     if (timeout < 0)
     {
-        return;
+      return -1;
     }
 
-    // 通信の開始の送信
-    I2C_GenerateSTART(I2C1, ENABLE);
+    // 1バイトのデータ送信
+    I2C_SendData(I2C1, data[i]);
+  }
 
-    // マスターモードに準備できるまで待つ
-    timeout = TIMEOUT_MAX;
-    while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT))
-        ;
-    if (timeout < 0)
-    {
-        return;
-    }
+  // 送信完了イベントを待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
+    ;
+  if (timeout < 0)
+  {
+    return -1;
+  }
 
-    // スレーブアドレスをマスターからスレーブへの通信として送信
-    I2C_Send7bitAddress(I2C1, address << 1, I2C_Direction_Transmitter);
+  // 通信の終了の送信
+  I2C_GenerateSTOP(I2C1, ENABLE);
 
-    // トランスミッターモードに準備できるまで待つ
-    timeout = TIMEOUT_MAX;
-    while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED))
-        ;
-    if (timeout < 0)
-    {
-        return;
-    }
-
-    for (int i = 0; i < length; i++)
-    {
-        // 1バイトのデータ送信
-        I2C_SendData(I2C1, data[i]);
-
-        // 送信完了イベントを待つ
-        timeout = TIMEOUT_MAX;
-        while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_TRANSMITTED))
-            ;
-        if (timeout < 0)
-        {
-            return;
-        }
-    }
-
-    // 通信の終了の送信
-    I2C_GenerateSTOP(I2C1, ENABLE);
+  return 0;
 }
 
 // 受信
-void read_i2c_data(uint8_t address, uint8_t *data, uint8_t length)
+int8_t read_i2c_data(uint8_t address, uint8_t *data, uint8_t length)
 {
-    int32_t timeout;
+  int32_t timeout;
 
-    // I2Cのバスがビジーでなくなるまで待つ
+  // I2Cのバスがビジーでなくなるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET)
+    ;
+  if (timeout < 0)
+  {
+    return -1;
+  }
+
+  // 通信の開始の送信
+  I2C_GenerateSTART(I2C1, ENABLE);
+
+  // マスターモードに準備できるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT))
+    ;
+  if (timeout < 0)
+  {
+    return -1;
+  }
+
+  // スレーブアドレスをマスターからスレーブへの通信として送信
+  I2C_Send7bitAddress(I2C1, address << 1, I2C_Direction_Receiver);
+
+  // マスターモードに準備できるまで待つ
+  timeout = TIMEOUT_MAX;
+  while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED))
+    ;
+  if (timeout < 0)
+  {
+    return -1;
+  }
+
+  for (int i = 0; i < length; i++)
+  {
+    // 受信完了イベントを待つ
     timeout = TIMEOUT_MAX;
-    while (timeout-- && I2C_GetFlagStatus(I2C1, I2C_FLAG_BUSY) != RESET)
-        ;
+    while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED))
+      ;
 
-    // 通信の開始の送信
-    I2C_GenerateSTART(I2C1, ENABLE);
-
-    // マスターモードに準備できるまで待つ
-    timeout = TIMEOUT_MAX;
-    while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT))
-        ;
-
-    // スレーブアドレスをマスターからスレーブへの通信として送信
-    I2C_Send7bitAddress(I2C1, address << 1, I2C_Direction_Receiver);
-
-    // マスターモードに準備できるまで待つ
-    timeout = TIMEOUT_MAX;
-    while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED))
-        ;
-
-    for (int i = 0; i < length; i++)
+    if (timeout < 0)
     {
-        // 受信完了イベントを待つ
-        timeout = TIMEOUT_MAX;
-        while (timeout-- && !I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_BYTE_RECEIVED))
-            ;
-
-        // 受信データの受信
-        data[i] = I2C_ReceiveData(I2C1);
+      return -1;
     }
 
-    // 通信の終了の送信
-    I2C_GenerateSTOP(I2C1, ENABLE);
+    // 受信データの受信
+    data[i] = I2C_ReceiveData(I2C1);
+  }
+
+  // 通信の終了の送信
+  I2C_GenerateSTOP(I2C1, ENABLE);
 }
 
 int main(void)
 {
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-    SystemCoreClockUpdate();
-    Delay_Init();
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+  SystemCoreClockUpdate();
+  Delay_Init();
 
-    // SDI printf の有効化
-    // ビルドフラグ SDI_PRINT=1 が必要
-    // wlink sdi-print enable を実行し、USBシリアルをリスンする
-    // SDI_Printf_Enable();
+  // SDI printf の有効化
+  // ビルドフラグ SDI_PRINT=1 が必要
+  // wlink sdi-print enable を実行し、USBシリアルをリスンする
+  // SDI_Printf_Enable();
 
-    // USART1 printf の有効化
-    USART_Printf_Init(115200);
+  // USART1 printf の有効化
+  USART_Printf_Init(115200);
 
-    Delay_Ms(100);
+  Delay_Ms(100);
 
-    printf("init\r\n");
+  printf("init\r\n");
 
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
-    I2C_InitTypeDef I2C_InitTSturcture = {0};
+  GPIO_InitTypeDef GPIO_InitStructure = {0};
+  I2C_InitTypeDef I2C_InitTSturcture = {0};
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
 #if USE_REMAP
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 #endif
 
 #if USE_REMAP
-    // I2Cのリマップ
-    // GPIO_PartialRemap_I2C1
-    // GPIO_FullRemap_I2C1
-    GPIO_PinRemapConfig(GPIO_FullRemap_I2C1, ENABLE);
-    // PC6: SDA, PC5: SCL
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_5;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
+  // I2Cのリマップ
+  // GPIO_PartialRemap_I2C1
+  // GPIO_FullRemap_I2C1
+  GPIO_PinRemapConfig(GPIO_FullRemap_I2C1, ENABLE);
+  // PC6: SDA, PC5: SCL
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
 #else
-    // PC1: SDA, PC2: SCL
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
+  // PC1: SDA, PC2: SCL
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
 #endif
 
-    // I2C1の設定
-    // クロック速度
-    I2C_InitTSturcture.I2C_ClockSpeed = 100000;
-    // モード（固定値）
-    I2C_InitTSturcture.I2C_Mode = I2C_Mode_I2C;
-    // デューティサイクル
-    I2C_InitTSturcture.I2C_DutyCycle = I2C_DutyCycle_2;
-    // Ackを有効にする
-    I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
-    // Ackアドレスのビット数
-    I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-    // 設定の有効化
-    I2C_Init(I2C1, &I2C_InitTSturcture);
+  // I2C1の設定
+  // クロック速度
+  I2C_InitTSturcture.I2C_ClockSpeed = 100000;
+  // モード（固定値）
+  I2C_InitTSturcture.I2C_Mode = I2C_Mode_I2C;
+  // デューティサイクル
+  I2C_InitTSturcture.I2C_DutyCycle = I2C_DutyCycle_2;
+  // Ackを有効にする
+  I2C_InitTSturcture.I2C_Ack = I2C_Ack_Enable;
+  // Ackアドレスのビット数
+  I2C_InitTSturcture.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+  // 設定の有効化
+  I2C_Init(I2C1, &I2C_InitTSturcture);
 
-    // I2Cの有効化
-    I2C_Cmd(I2C1, ENABLE);
-    I2C_AcknowledgeConfig(I2C1, ENABLE);
+  // I2Cの有効化
+  I2C_Cmd(I2C1, ENABLE);
+  I2C_AcknowledgeConfig(I2C1, ENABLE);
 
-    Delay_Ms(100);
+  Delay_Ms(100);
 
-    printf("first send\r\n");
+  printf("first send\r\n");
 
-    uint8_t buf1[2] = {0x30, 0xA2};
-    send_i2c_data(SHT31_I2C_ADDR, buf1, 2);
+  uint8_t buf1[2] = {0x30, 0xA2};
+  send_i2c_data(SHT31_I2C_ADDR, buf1, 2);
+  Delay_Ms(300);
+
+  uint8_t buf2[2] = {0x30, 0x41};
+  send_i2c_data(SHT31_I2C_ADDR, buf2, 2);
+  Delay_Ms(300);
+
+  printf("start\r\n");
+
+  int count = 0;
+
+  while (1)
+  {
+    printf("loop %d\r\n", count++);
+
+    uint8_t dac[6];
+    uint32_t t, h;
+    uint32_t temperature, humidity;
+
+    uint8_t buf[2] = {0x24, 0x00};
+    send_i2c_data(SHT31_I2C_ADDR, buf, 2);
+
     Delay_Ms(300);
 
-    uint8_t buf2[2] = {0x30, 0x41};
-    send_i2c_data(SHT31_I2C_ADDR, buf2, 2);
-    Delay_Ms(300);
+    read_i2c_data(SHT31_I2C_ADDR, dac, sizeof(dac));
 
-    printf("start\r\n");
+    t = (dac[0] << 8) | dac[1];                       // 1Byte目のデータを8bit左にシフト、OR演算子で2Byte目のデータを結合して、tに代入
+    temperature = (((uint32_t)(t) * 175) >> 16) - 45; // 温度の計算、temperatureに代入
+    h = (dac[3] << 8) | dac[4];                       // 4Byte目のデータを8bit左にシフト、OR演算子で5Byte目のデータを結合して、hに代入
+    humidity = (((uint32_t)(h) * 100) >> 16);         // 湿度の計算、humidityに代入
 
-    int count = 0;
+    printf("Temperature: %lu, Humidity: %lu\r\n", temperature, humidity);
 
-    while (1)
-    {
-        printf("loop %d\r\n", count++);
-
-        uint8_t dac[6];
-        uint32_t t, h;
-        uint32_t temperature, humidity;
-
-        uint8_t buf[2] = {0x24, 0x00};
-        send_i2c_data(SHT31_I2C_ADDR, buf, 2);
-
-        Delay_Ms(300);
-
-        read_i2c_data(SHT31_I2C_ADDR, dac, sizeof(dac));
-
-        t = (dac[0] << 8) | dac[1];                       // 1Byte目のデータを8bit左にシフト、OR演算子で2Byte目のデータを結合して、tに代入
-        temperature = (((uint32_t)(t) * 175) >> 16) - 45; // 温度の計算、temperatureに代入
-        h = (dac[3] << 8) | dac[4];                       // 4Byte目のデータを8bit左にシフト、OR演算子で5Byte目のデータを結合して、hに代入
-        humidity = (((uint32_t)(h) * 100) >> 16);         // 湿度の計算、humidityに代入
-
-        printf("Temperature: %lu, Humidity: %lu\r\n", temperature, humidity);
-
-        Delay_Ms(5000);
-    }
+    Delay_Ms(5000);
+  }
 }
 
 void NMI_Handler(void) {}
 void HardFault_Handler(void)
 {
-    while (1)
-    {
-    }
+  while (1)
+  {
+  }
 }
