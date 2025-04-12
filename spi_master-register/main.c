13faf207063e0212b990a1850c26e3c9b558cdfe
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-04-12 17:17:50 +0900

    コードの整形とコメントの追加による可読性の向上

--- spi_master-register/main.c
+++ spi_master-register/main.c
@@ -6,132 +6,133 @@
 
 void init_rcc(void)
 {
-	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_SPI1;
+  RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_SPI1;
 }
 
 void init_spi_master()
 {
 
-	// PC1 is NSS
-	GPIOC->CFGLR &= ~(0xf << (4 * 1));
-	GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 1);
+  // PC1: NSS
+  GPIOC->CFGLR &= ~(0xf << (4 * 1));
+  GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 1);
 
-	// PC5 is SCK
-	GPIOC->CFGLR &= ~(0xf << (4 * 5));
-	GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 5);
+  // PC5: SCK
+  GPIOC->CFGLR &= ~(0xf << (4 * 5));
+  GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 5);
 
-	// PC6 is MOSI
-	GPIOC->CFGLR &= ~(0xf << (4 * 6));
-	GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 6);
+  // PC6: MOSI
+  GPIOC->CFGLR &= ~(0xf << (4 * 6));
+  GPIOC->CFGLR |= (GPIO_Speed_50MHz | GPIO_CNF_OUT_PP_AF) << (4 * 6);
 
-	// PC7 is MISO
-	GPIOC->CFGLR &= ~(0xf << (4 * 7));
-	GPIOC->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * 7);
+  // PC7: MISO
+  GPIOC->CFGLR &= ~(0xf << (4 * 7));
+  GPIOC->CFGLR |= (GPIO_Speed_In | GPIO_CNF_IN_FLOATING) << (4 * 7);
 
-	uint16_t tempreg;
+  uint16_t tempreg;
 
-	// Reset SPI1
-	SPI1->CTLR1 = 0;
+  // SPI設定
+  // SPI設定をリセット
+  SPI1->CTLR1 = 0;
+  SPI1->CTLR1 |=
+      // プリスケーラ
+      (SPI_CTLR1_BR & SPI_BaudRatePrescaler_32) |
+      // SPIモード
+      SPI_CPOL_Low | SPI_CPHA_1Edge |
+      // NSSの制御モード
+      SPI_NSS_Hard |
+      SPI_Mode_Master;
 
-	// プリスケーラ
-	SPI1->CTLR1 |= SPI_CTLR1_BR & SPI_BaudRatePrescaler_32;
-
-	SPI1->CTLR1 |= SPI_CPOL_Low | SPI_CPHA_1Edge;
-
-	SPI1->CTLR1 |= SPI_NSS_Hard;
-	SPI1->CTLR2 |= SPI_CTLR2_SSOE;
-
-	SPI1->CTLR1 |= SPI_Mode_Master;
-	// SPI1->CTLR1 |= CTLR1_SPE_Set;
+  // SPIの開始
+  SPI1->CTLR2 |= SPI_CTLR2_SSOE;
 }
 
 void spi_begin_8()
 {
-	SPI1->CTLR1 &= ~(SPI_CTLR1_DFF);
-	SPI1->CTLR1 |= SPI_CTLR1_SPE;
+  SPI1->CTLR1 &= ~(SPI_CTLR1_DFF);
+  SPI1->CTLR1 |= SPI_CTLR1_SPE;
 }
 
 void spi_end()
 {
-	SPI1->CTLR1 &= ~(SPI_CTLR1_SPE);
+  SPI1->CTLR1 &= ~(SPI_CTLR1_SPE);
 }
 
 uint8_t spi_transfer(uint8_t data)
 {
-	SPI1->DATAR = data;
-	while (!(SPI1->STATR & SPI_STATR_TXE))
-		;
+  while (!(SPI1->STATR & SPI_STATR_TXE))
+    ;
+  SPI1->DATAR = data;
 
-	while (!(SPI1->STATR & SPI_STATR_RXNE))
-		;
-	return SPI1->DATAR;
+  while (!(SPI1->STATR & SPI_STATR_RXNE))
+    ;
+  return SPI1->DATAR;
 }
 
 int main()
 {
-	SystemInit();
+  SystemInit();
 
-	printf("init\r\n");
+  printf("init\r\n");
 
-	init_rcc();
+  init_rcc();
 
-	init_spi_master();
+  init_spi_master();
 
-	printf("SPI1->CTLR1 %04x\r\n", SPI1->CTLR1);
-	printf("SPI1->CTLR2 %04x\r\n", SPI1->CTLR2);
-	printf("GPIOC->CFGLR %08x\r\n", GPIOC->CFGLR);
+  printf("SPI1->CTLR1 %04x\r\n", SPI1->CTLR1);
+  printf("SPI1->CTLR2 %04x\r\n", SPI1->CTLR2);
+  printf("GPIOC->CFGLR %08x\r\n", GPIOC->CFGLR);
 
-	printf("setup\r\n");
+  printf("setup\r\n");
 
-	Delay_Ms(100);
+  Delay_Ms(100);
 
-	spi_begin_8();
+  spi_begin_8();
 
-	spi_transfer(0xFF);
-	spi_transfer(0xFF);
-	spi_transfer(0xFF);
-	spi_transfer(0xFF);
+  spi_transfer(0xFF);
+  spi_transfer(0xFF);
+  spi_transfer(0xFF);
+  spi_transfer(0xFF);
 
-	Delay_Ms(100);
+  Delay_Ms(100);
 
-	spi_transfer(0x54);
+  spi_transfer(0x54);
 
-	spi_end();
+  spi_end();
 
-	Delay_Ms(500);
+  Delay_Ms(500);
 
-	while (1)
-	{
-		printf("loop\r\n");
+  while (1)
+  {
+    printf("loop\r\n");
 
-		spi_begin_8();
+    spi_begin_8();
 
-		uint8_t b1 = spi_transfer(0x00);
-		uint8_t b2 = spi_transfer(0x00);
+    uint8_t b1 = spi_transfer(0x00);
+    uint8_t b2 = spi_transfer(0x00);
 
-		spi_end();
+    spi_end();
 
-		uint16_t raw = b1 << 8 | b2;
-		printf("raw: %04x\r\n", raw);
+    uint16_t raw = b1 << 8 | b2;
+    printf("raw: %04x\r\n", raw);
 
-		raw = raw >> 3;
+    raw = raw >> 3;
 
-		int32_t raw_int;
+    int32_t raw_int;
 
-		if (raw & 0x1000)
-		{
-			raw_int = raw - 0x2000;
-		}
-		else
-		{
-			raw_int = raw;
-		}
+    if (raw & 0x1000)
+    {
+      raw_int = raw - 0x2000;
+    }
+    else
+    {
+      raw_int = raw;
+    }
 
-		printf("raw_int: %d\r\n", raw_int);
-		int32_t int_temp = raw_int >> 4;
-		int32_t frac_temp = raw_int & 0x0f;
-		printf("temp:%3d.%04d\r\n", int_temp, frac_temp * 625);
+    printf("raw_int: %d\r\n", raw_int);
+    int32_t int_temp = raw_int >> 4;
+    int32_t frac_temp = raw_int & 0x0f;
+    printf("temp:%3d.%04d\r\n", int_temp, frac_temp * 625);
 
-		Delay_Ms(1000);
-	}
+    Delay_Ms(1000);
+  }
 }
