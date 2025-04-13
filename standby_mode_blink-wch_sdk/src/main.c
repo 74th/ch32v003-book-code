5406b68c8bbd7ae3fb79f8e56c6d34d2bc7a50a1
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-04-13 11:00:59 +0900

    コードの整形と可読性の向上のため、インデントを統一し、コメントを整理

--- standby_mode_blink-wch_sdk/src/main.c
+++ standby_mode_blink-wch_sdk/src/main.c
@@ -14,85 +14,90 @@ void Delay_Ms(uint32_t n);
 
 int main(void)
 {
-    GPIO_InitTypeDef GPIO_InitStructure = {0};
-    EXTI_InitTypeDef EXTI_InitStructure = {0};
+  GPIO_InitTypeDef GPIO_InitStructure = {0};
+  EXTI_InitTypeDef EXTI_InitStructure = {0};
 
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-    SystemCoreClockUpdate();
-    Delay_Init();
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+  SystemCoreClockUpdate();
+  Delay_Init();
 
 #if ENABLE_UART_PRINT
+  USART_Printf_Init(115200);
+
+  printf("wait\r\n");
+#endif
+
+  Delay_Ms(5000);
+
+#if ENABLE_UART_PRINT
+  printf("init\r\n");
+#endif
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+  RCC_LSICmd(ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = BLINKY_GPIO_PIN;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+  GPIO_Init(BLINKY_GPIO_PORT, &GPIO_InitStructure);
+
+  EXTI_InitStructure.EXTI_Line = EXTI_Line9;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Event;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStructure);
+
+  while (RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET)
+    ;
+
+  // LSI 128kHz
+  // Prescaler 10240 -> 128kHz / 10240 = 12.5Hz = 80ms
+  // window value 31
+  // 80ms * 31 = 2000ms = 2s
+  PWR_AWU_SetPrescaler(PWR_AWU_Prescaler_10240);
+  PWR_AWU_SetWindowValue(25);
+  // Prescaler 61440 -> 128kHz / 61440 = 2Hz = 500ms
+  // window value 4
+  // 500ms * 4 = 2000ms = 2s
+  // PWR_AWU_SetPrescaler(PWR_AWU_Prescaler_61440);
+  // PWR_AWU_SetWindowValue(4);
+  PWR_AutoWakeUpCmd(ENABLE);
+
+  GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, 1);
+
+#if ENABLE_UART_PRINT
+  printf("start\r\n");
+#endif
+
+  int count = 0;
+  uint8_t ledState = 0;
+
+  while (1)
+  {
+#if ENABLE_UART_PRINT
+    printf("go to standby: %d\r\n\r\n", count);
+#endif
+
+    PWR_EnterSTANDBYMode(PWR_STANDBYEntry_WFE);
+
     USART_Printf_Init(115200);
 
-    printf("wait\r\n");
-#endif
-
-    Delay_Ms(5000);
-
 #if ENABLE_UART_PRINT
-    printf("init\r\n");
+    printf("\r\nAuto wake up: %d \r\n", count);
 #endif
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
-    RCC_LSICmd(ENABLE);
+    GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, ledState);
+    ledState ^= 1;
 
-    GPIO_InitStructure.GPIO_Pin = BLINKY_GPIO_PIN;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIO_Init(BLINKY_GPIO_PORT, &GPIO_InitStructure);
-
-    EXTI_InitStructure.EXTI_Line = EXTI_Line9;
-    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Event;
-    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
-    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
-    EXTI_Init(&EXTI_InitStructure);
-
-    while (RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET)
-        ;
-
-    // LSI 128kHz
-    // Prescaler 10240 -> 128kHz / 10240 = 12.5Hz = 80ms
-    // window value 31
-    // 80ms * 31 = 2000ms = 2s
-    PWR_AWU_SetPrescaler(PWR_AWU_Prescaler_10240);
-    PWR_AWU_SetWindowValue(25);
-    PWR_AutoWakeUpCmd(ENABLE);
-
-    GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, 1);
-
-#if ENABLE_UART_PRINT
-    printf("start\r\n");
-#endif
-
-    int count = 0;
-    uint8_t ledState = 0;
-
-    while (1)
-    {
-#if ENABLE_UART_PRINT
-        printf("go to standby: %d\r\n\r\n", count);
-#endif
-
-        PWR_EnterSTANDBYMode(PWR_STANDBYEntry_WFE);
-
-        USART_Printf_Init(115200);
-
-#if ENABLE_UART_PRINT
-        printf("\r\nAuto wake up: %d \r\n", count);
-#endif
-
-        GPIO_WriteBit(BLINKY_GPIO_PORT, BLINKY_GPIO_PIN, ledState);
-        ledState ^= 1;
-
-        count++;
-    }
+    count++;
+  }
 }
 
 void NMI_Handler(void) {}
 void HardFault_Handler(void)
 {
-    while (1)
-    {
-    }
+  while (1)
+  {
+  }
 }
